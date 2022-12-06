e8a38e4e5c8b7eebed45ff632c8571bb44cb3d5f
beescp7 <bolgun860@gmail.com>
2022-12-06 22:42:17 +0300

    Yazılım Geliştirici Yetiştirme Kampı 13. Gün

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Castle.DynamicProxy.Generators;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,53 +21,62 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryServise _categoryServise;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal,ICategoryServise categoryServise)
         {
             _productDal = productDal;
+            _categoryServise = categoryServise;
         }
+
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //iş kodları
-            //Yetkisi Var Mı?
-            //magic strings 
-            //validation yapısal olarak uygun mu onu kontrol et min max degerler vb
-          
+            IResult result= BusinessRules.Run(
+                                    CheckIfProductNameExists(product.ProductName),
+                                    CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+                                    CheckIfCategoryLimitExceded());
+
+            if (result!=null)
+            {
+                return result;
+            }
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
+
+
         }
 
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
             //Yetkisi Var Mı?
-            if(DateTime.Now.Hour==14)
+            if (DateTime.Now.Hour == 14)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintemamceTime);
             }
 
-            return new SuccessDataResult<List<Product>>( _productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
             //iş kodları
             //Yetkisi Var Mı?
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id)); 
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
-             
+
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
-          
+
 
         }
 
@@ -75,7 +86,41 @@ namespace Business.Concrete
             {
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintemamceTime);
             }
-            return  new SuccessDataResult<List <ProductDetailDto>>(_productDal.GetProductDetails());
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
+    
+      private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if(result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreaddyExists);
+            }
+            return new SuccessResult();
+            
+        }
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryServise.GetAll();
+            if (result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
+        }
+    
+
     }
+
+
+     
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,5 +13,9 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintemamceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "kategori sayısı 10 u gecemez ";
+        public static string ProductNameAlreaddyExists="Bu isimde zaten ürün var ";
+        public static string CategoryLimitExceded="category limitin doldu kirvem";
     }
 }
